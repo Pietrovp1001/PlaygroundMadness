05077acbccb02fbf47547446e5926da2e3c6d2d0
Pietrovp1001 <pietro.villano@upb.edu.co>
2022-08-03 00:45:12 -0500

    Procedual Map Generation Basic

---
+++ PlaygroundMadness/Assets/Scripts/Map Scripts/ReloadScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadScene : MonoBehaviour {
+
+	void Update () {//reload scene, for testing purposes
+		if (Input.GetKeyDown("r")){
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
+}
