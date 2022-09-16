ebcc262e79eaab3be3318ebe5eb1d0d31b5cb728
Pietrovp1001 <pietro.villano@upb.edu.co>
2022-09-16 04:38:39 -0500

    Boss camera fix

--- PlaygroundMadness/Assets/Scripts/TriggerSpawner.cs
+++ PlaygroundMadness/Assets/Scripts/TriggerSpawner.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Mime;
 using System.Transactions;
 using MoreMountains.TopDownEngine;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,24 +15,33 @@ public class TriggerSpawner : MonoBehaviour
     private bool used = false;
     public GameObject locked;
     public GameObject playerPos;
-    
+
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.CompareTag("Player")) {
+        if (col.gameObject.CompareTag("Player") && used == false)
+        {
+            RoomCounter rooms = col.GetComponent<RoomCounter>();
             playerPos.SetActive(true);
-            if (used == false) {
-                RoomCounter rooms = col.GetComponent<RoomCounter>();
-                if (rooms.roomCount > 0) 
-                    Spawner.SpawnObject();
+            if (rooms.roomCount > 0)
+            {
+                Spawner.SpawnObject();
                 ObstacleSpawner.SpawnObstacle();
                 used = true;
                 rooms.roomCount++;
                 locked.SetActive(false);
             }
+            else
+            {
+                rooms.roomCount++;
+                used = true;
+            }
         }
     }
-    private void OnTriggerExit2D(Collider2D col) {
-        if (col.gameObject.CompareTag("Player")) {
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
             playerPos.SetActive(false);
         }
     }
