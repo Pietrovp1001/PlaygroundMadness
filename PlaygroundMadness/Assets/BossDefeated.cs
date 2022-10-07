8d8849672db56f6ec172f3e04db83dbe67afc5c8
Pietrovp1001 <pietro.villano@upb.edu.co>
2022-10-06 23:12:17 -0500

    Boss finished

---
+++ PlaygroundMadness/Assets/BossDefeated.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.TopDownEngine;
+using UnityEngine;
+
+public class BossDefeated : MonoBehaviour
+{
+    public GameObject portal;
+    public GameObject boss;
+    public ParticleSystem particles;
+    
+    private void Update()
+    {
+        if (boss.GetComponent<Health>().CurrentHealth == 0)
+        {
+            portal.SetActive(true);
+            particles.Play();
+            Destroy(particles, 1f);
+        }
+    }
+ 
+    
+    
+}
---
+++ PlaygroundMadness/Assets/Scripts/SpawnEnemiesBoss.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.TopDownEngine;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpawnEnemiesBoss : MonoBehaviour
+{
+    public List<GameObject> enemies = new List<GameObject>();
+    public GameObject boss;
+    private void Update()
+    {
+        if (boss.GetComponent<Health>().CurrentHealth <= 90)
+        {
+           SpawnEnemies();
+           enemies.Clear();
+        }
+    }
+    
+    public void SpawnEnemies()
+    {
+        foreach (var spawnObject in enemies)
+        {
+            GameObject instanceObject = Instantiate(spawnObject, boss.transform.position, quaternion.identity) as GameObject;
+           
+        }
+    }    
+    
+}
